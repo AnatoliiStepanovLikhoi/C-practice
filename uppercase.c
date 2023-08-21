0cadd38fc5622297e3c1a9a5822e1accfaab77b0
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-08-21 13:57:32 +0300

    init: first commit

---
+++ uppercase.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <ctype.h>
+#include <string.h>
+
+int main(void)
+{
+    string s = get_string("Before: ");
+
+    printf("After: ");
+
+     int n = strlen(s);
+
+    for(int i = 0; i < n; i++)
+    {
+        printf("%c", toupper(s[i]));
+    }
+
+    printf("\n");
+}

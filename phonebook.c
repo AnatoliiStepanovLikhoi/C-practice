70635a3c5dc7acfc6a79982b8a66f58dd97730a6
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-08-23 11:29:38 +0300

    add: search & phonebook

---
+++ phonebook.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+typedef struct
+{
+    string name;
+    string number;
+}
+person;
+
+int main(void)
+{
+    person people[2];
+
+    people[0].name = "Carter";
+    people[0].number = "+380678976655";
+
+    people[1].name = "David";
+    people[1].number = "+380675674433";
+
+    string name = get_string("Name: ");
+
+    for (int i = 0; i < 2; i++)
+    {
+        if(strcmp(people[i].name, name) == 0)
+        {
+            printf("Found %s\n",people[i].number);
+            return 0;
+        }
+    }
+
+    printf("Not Found\n");
+    return 1;
+
+}
